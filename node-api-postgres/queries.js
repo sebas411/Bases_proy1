6d4b36fa61555fe2ab1d408194822e76884e510d
sebas411 <59925024+sebas411@users.noreply.github.com>
2021-03-23 22:28:35 -0600

    update javascript

--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -8,13 +8,13 @@ const pool = new Pool({
 })
 
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM usuario ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
+  pool.query('SELECT * FROM usuario ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
 module.exports = {
     getUsers
